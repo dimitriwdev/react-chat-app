1953357be7a67faefee3dbc63806f6e2c486af16
Dimitri Devoille <dimitri.wdev@gmail.com>
2021-10-17 13:07:37 +0100

    add chat app and local storage for user data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm';
+import './style.css'
 
-function App() {
+
+
+const App = () => {
+
+  const projectId = process.env.REACT_APP_KEY;
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
-    <div className="App">
-
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectId}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
+
+
 
 export default App;
